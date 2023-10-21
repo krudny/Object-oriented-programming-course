394a2dfbf88cf3ec4a1f93314b63eee8f25e6d06
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-10-21 16:20:42 +0200

    Created MoveDirections

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("System zaczął działanie");
+        run()
+        System.out.println("System skończył działanie");
+    }
+}
