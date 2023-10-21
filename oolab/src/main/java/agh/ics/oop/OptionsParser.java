1fd93938d88ff6f5046245f19867084ab88f900d
Kamil Rudny <134285490+krudny@users.noreply.github.com>
2023-10-21 16:27:40 +0200

    Finished OptionsParser

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,24 @@
+package agh.ics.oop;
+import agh.ics.oop.model.MoveDirection;
+
+import java.util.Objects;
+
+public class OptionsParser {
+    public static MoveDirection[] Parser(String[] args) {
+        MoveDirection[] moveDirection = new MoveDirection[args.length];
+
+        for(int i = 0; i < args.length; i++){
+            if (Objects.equals(args[i], "f")) {
+                moveDirection[i] = MoveDirection.FORWARD;
+            } else if (Objects.equals(args[i], "b")) {
+                moveDirection[i] = MoveDirection.BACKWARDS;
+            } else if (Objects.equals(args[i], "l")) {
+                moveDirection[i] = MoveDirection.LEFT;
+            } else if (Objects.equals(args[i], "r")) {
+                moveDirection[i] = MoveDirection.RIGHT;
+            }
+        }
+
+        return moveDirection;
+    }
+}
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,9 +1,17 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
+
+    public static void run(MoveDirection[] directions) {
+        for (MoveDirection direction : directions) {
+            System.out.println(direction);
+        }
+    }
     public static void main(String[] args) {
         System.out.println("System zaczął działanie");
-        run()
+        run(OptionsParser.Parser(args));
         System.out.println("System skończył działanie");
     }
 }
